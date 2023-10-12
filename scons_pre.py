935a12d5eb0d8551caea3479f295ccc0facfb6f6
Bruce Oberg <bruce@oberg.org>
2023-10-12 16:43:35 -0700

    export project (dir) name to C code

--- scons_pre.py
+++ scons_pre.py
@@ -1,8 +1,15 @@
-# extra script to expose platformio.ini values to C++ code
+# extra script to expose platformio.ini values (and other stuff) to C++ code
 
-env = DefaultEnvironment()
+from pathlib import Path
 
-env.Append(CPPDEFINES=[
-	("MONITOR_SPEED", env.GetProjectOption("monitor_speed")),
-	("UPLOAD_VIA_ESP_PROG", 1 if env.GetProjectOption("upload_protocol") == "esp-prog" else 0),
-])
+env = DefaultEnvironment() # pyright: ignore [reportUndefinedVariable]
+
+strProject = Path(env.Dir('#').abspath).name
+
+env.Append(
+	CPPDEFINES=[
+		("PROJECT_NAME", env.StringifyMacro(strProject)),
+		("MONITOR_SPEED", env.GetProjectOption("monitor_speed")),
+		("UPLOAD_VIA_ESP_PROG", 1 if env.GetProjectOption("upload_protocol") == "esp-prog" else 0),
+	]
+)
