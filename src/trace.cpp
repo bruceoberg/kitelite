512e8414e9dcbea6aee3f38cea50bc7c7034669a
Bruce Oberg <bruce@oberg.org>
2023-11-18 17:42:43 -0800

    s3 feathers can't redirect Serial

--- src/trace.cpp
+++ src/trace.cpp
@@ -9,6 +9,7 @@ using namespace Trace;
 
 void Trace::Startup()
 {
+#if PLAT_FEATHER_V2
 	// NOTE bruceo: when using the ESP-PROG board, serial output can go to either the onboard
 	//	USB port or via the onboard UART pins though the ESP-PROG "PROG" interface.
 
@@ -18,6 +19,12 @@ void Trace::Startup()
 	static const U32 s_configDefault = SERIAL_8N1;	// from arduino's HardwareSerial::begin() declaration
 
 	Serial.begin(MONITOR_SPEED, s_configDefault, pinRx, pinTx);
+#else // !PLAT_FEATHER_V2
+	// NOTE bruceo: still investigating ESP-PROG on S3 feathers.
+
+	Serial.begin(MONITOR_SPEED);
+#endif // !PLAT_FEATHER_V2
+
 	while (!Serial) {
 		delay(100); // wait for native usb
 	}
