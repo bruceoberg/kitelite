4a84d5aee1fe320b32bd34eb2a19d5433643e2be
Bruce Oberg <bruce@oberg.org>
2023-11-18 17:40:51 -0800

    platform/tft defines

--- include/common.h
+++ include/common.h
@@ -2,6 +2,33 @@
 
 #include <Arduino.h>
 
+#if defined(ARDUINO_ADAFRUIT_FEATHER_ESP32_V2)
+	#define PLAT_FEATHER_V2 1
+	#define PLAT_FEATHER_S3 0
+	#define PLAT_FEATHER_S3_REVTFT 0
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S3)
+	#define PLAT_FEATHER_V2 0
+	#define PLAT_FEATHER_S3 1
+	#define PLAT_FEATHER_S3_REVTFT 0
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S3_REVTFT)
+	#define PLAT_FEATHER_V2 0
+	#define PLAT_FEATHER_S3 0
+	#define PLAT_FEATHER_S3_REVTFT 1
+#else
+#error Unknown board
+#endif
+
+#if defined(FORCE_ENABLE_TFT)
+	#undef ENABLE_TFT
+	#define ENABLE_TFT FORCE_ENABLE_TFT
+#endif // FORCE_ENABLE_TFT
+
+#if !defined(ENABLE_TFT)
+	#define ENABLE_TFT PLAT_FEATHER_S3_REVTFT
+#endif // !defined(ENABLE_TFT)
+
+
+
 // common types & macros
 
 using U8 =	uint8_t;
